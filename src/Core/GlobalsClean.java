3e7df7fe82fc3c4d5def5fd00b9214acd2085a9c
Mark Lubberts <mlubberts@outlook.com>
2013-09-27 21:31:21 +0200

    Generated a CLEAN Globals file (Old one had a useless username / password combination)
    Signed-off-by: Mark Lubberts <mlubberts@outlook.com>

---
+++ src/Core/GlobalsClean.java
@@ -0,0 +1,23 @@
+package Core;
+
+public class GlobalsClean 
+{
+    // CHANGE THE FILENAME TO GLOBALS.
+    // ALSO CHANGE THE CLASSNAME TO GLOBALS.
+    
+    // Bot initialization details.
+    public static final String USERNAME = "USERNAME";
+    public static final String PASSWORD = "PASSWORD";
+    public static final String VERSTEXT = "VERSION TEXT";
+    
+    // MySQL connection details
+    public static final String MSQLHOST = "MYSQL HOST";
+    public static final String MSQLUSER = "MYSQL USER";
+    public static final String MSQLPASS = "MYSQL PASS";
+    public static final String MSQLPORT = "MYSQL PORT";
+    public static final String MSQLDATA = "DATABASE NAME";
+    
+    // Level game settings
+    public static final Double INCREMENTS = 0.5;
+    public static final int    EXPERIENCE = 10;
+}
